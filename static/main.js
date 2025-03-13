d6f02a1318dc4f525bf6210334dbf5e61c86eecf
nastycify <bashtetskayanastya@gmail.com>
2025-03-13 20:38:12 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -130,7 +130,7 @@ psychoJS.start({
   expName: expName,
   expInfo: expInfo,
   resources: [
-    {'name': 'Cat8.jpg', 'path': 'static/Cat8.png'},
+    {'name': 'Cat8.jpg', 'path': '/static/Cat8.png'},
     {'name': 'Stimul_1.xlsx', 'path': 'static/Stimul_1.xlsx'},
     {'name': 'Stimul_2.xlsx', 'path': 'static/Stimul_2.xlsx'},
     {'name': 'Stimul_3.xlsx', 'path': 'static/Stimul_3.xlsx'},
@@ -458,7 +458,7 @@ async function experimentInit() {
   image_2 = new visual.ImageStim({
     win : psychoJS.window,
     name : 'image_2', units : undefined, 
-    image : 'static/Cat8.png', mask : undefined,
+    image : '/static/Cat8.png', mask : undefined,
     anchor : 'center',
     ori : 0.0, pos : [0, 0], size : [1, 1],
     color : new util.Color([(- 1.0), (- 1.0), (- 1.0)]), opacity : undefined,
